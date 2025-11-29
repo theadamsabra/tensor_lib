ed34bb2b51e614d76f95a5ca70a7ec580fd5df1f
theadamsabra <theadamsabra@gmail.com>
2025-11-28 19:52:17 -0800

    work on tensor op test

--- include/tensor.h
+++ include/tensor.h
@@ -10,12 +10,11 @@
 using namespace std;
 
 template <typename T>
-
 using Data = vector<T>;
+
 using Shape = vector<size_t>;
 using Strides = vector<size_t>;
 using Index = vector<size_t>;
-using N = size_t;
 
 template <typename T>
 class Tensor 
@@ -54,79 +53,74 @@ private:
     };
 
 public:
-    // Default constructor is an emtpy tensor of specified shape:
+    // Constructor for when shape and data is defined:
+    Tensor(const Shape s, const Data<T> d) : shape(s), data(d)
+    {
+        size_t total_size = accumulate(shape.begin(), shape.end(), 1, multiplies<size_t>());
+        // TODO: Check shape and data are same size.
+        calculate_strides();
+    };
+
+    // Constructor is an emtpy tensor of specified shape:
     Tensor(const Shape shape) : shape(shape)
     {
         calculate_strides();
         size_t total_size = accumulate(shape.begin(), shape.end(), 1, multiplies<size_t>());
-        data.resize(total_size);
+        data.resize(total_size, 0);
     };
 
     Shape get_shape(){
         return shape;
     };
+
     // Overload indexing:
-    T& operator[](Index& index){
+    T operator[](Index &index){
         size_t n = index.size();
         int global_idx = 0; 
         for (int i=0; i<n; i++){
+            // TODO: Check why stride isn't being constructed
             global_idx += index[i] * stride[i];
         };
         return data[global_idx];
     };
 
-    // Read-only indexing:
-    const T& operator[](Index& index) const {
-        size_t n = index.size();
-        int global_idx = 0; 
-        for (int i=0; i<n; i++){
-            global_idx += index[i] * stride[i];
-        };
-        return data[global_idx];
-    };
+    // // Overload addition
+    // Tensor<T&> operator+(Tensor<T&> t){
+    //     // Verify shape is equal:
+    //     Shape self_shape = shape; 
+    //     Shape additive_shape = t.get_shape();
+    //     assert(self_shape == additive_shape);
 
-    // Overload addition
-    Tensor<T&> operator+(Tensor<T&> t){
-        // Verify shape is equal:
-        Shape self_shape = shape; 
-        Shape additive_shape = t.get_shape();
-        assert(self_shape == additive_shape);
+    //     // Verify rank is equal:
+    //     size_t n = self_shape.size();
+    //     size_t m = additive_shape.size();
+    //     assert(n == m);
 
-        // Verify rank is equal:
-        size_t n = self_shape.size();
-        size_t m = additive_shape.size();
-        assert(n == m);
+    //     Tensor<T> output(self_shape);
+    //     // Add by simple indexing across entire vector:
+    //     for (int i=0; i<data.size(); i++){
+    //         output[i] = data[i] + additive_shape[i];
+    //     };
+    //     return output;
+    // };
 
-        Tensor<T> output(self_shape);
-        // Add by simple indexing across entire vector:
-        for (int i=0; i<data.size(); i++){
-            output[i] = data[i] + additive_shape[i];
-        };
-        return output;
-    };
+    // // Overload multiplication 
+    // Tensor<T&> operator*(Tensor<T&> t){
+    //     // Verify shape is equal:
+    //     Shape self_shape = shape; 
+    //     Shape additive_shape = t.get_shape();
+    //     assert(self_shape == additive_shape);
 
-    // Overload multiplication 
-    Tensor<T&> operator*(Tensor<T&> t){
-        // Verify shape is equal:
-        Shape self_shape = shape; 
-        Shape additive_shape = t.get_shape();
-        assert(self_shape == additive_shape);
+    //     // Verify rank is equal:
+    //     size_t n = self_shape.size();
+    //     size_t m = additive_shape.size();
+    //     assert(n == m);
 
-        // Verify rank is equal:
-        size_t n = self_shape.size();
-        size_t m = additive_shape.size();
-        assert(n == m);
-
-        Tensor<T> output(self_shape);
-        // Add by simple indexing across entire vector:
-        for (int i=0; i<data.size(); i++){
-            output[i] = data[i] * additive_shape[i];
-        };
-        return output;
-    };
-};
-
-int main(){
-    Tensor<float> t({2,3,4});
-    return 0;
+    //     Tensor<T> output(self_shape);
+    //     // Add by simple indexing across entire vector:
+    //     for (int i=0; i<data.size(); i++){
+    //         output[i] = data[i] * additive_shape[i];
+    //     };
+    //     return output;
+    // };
 };
--- tests/test_tensor_ops.cpp
+++ tests/test_tensor_ops.cpp
@@ -1,9 +1,22 @@
-#include <iostream>
-#include <cassert>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "tensor.h"
-
+#include "doctest.h"
+#include <iostream>
 using namespace std;
 
-int main(){
-    return 0;
-}
+TEST_CASE("Tensor Construction (Shape Only)"){
+    Tensor<int> a({2});
+};
+
+TEST_CASE("Tensor Construction (Shape and Data)"){
+    Tensor<int> a({2}, {1, 2});
+};
+
+TEST_CASE("Tensor Indexing"){
+    Tensor<int> a({2}, {1, 2});
+    Index zeroth = {0};
+    Index first = {1};
+
+    CHECK(a[zeroth] == 1);
+    CHECK(a[first] == 2);
+};
